fd9f80e510423cecae259baf3b080f8d4fdebbe0
prathameshc18 <53288104+prathameshc18@users.noreply.github.com>
2019-07-25 18:34:06 +0530

    Second game

---
+++ First attempt/Second game/Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+        {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(600 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-600 * Time.deltaTime, 0, 0);
+        }
+    }
+}
