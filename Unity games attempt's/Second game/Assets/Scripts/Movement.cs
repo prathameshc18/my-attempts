01c310623eaa7a2c02956e2448b609efd4460d99
prathameshc18 <53288104+prathameshc18@users.noreply.github.com>
2019-07-26 08:53:59 +0530

    Score

---
+++ Unity games attempt's/Second game/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 200f;
+    public float sideForce = 1f;
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+        {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
---
+++ Unity games attempt's/Second game/Assets/Scripts/collision.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision : MonoBehaviour
+{
+    public Movement movement;
+    void OnCollisionEnter(UnityEngine.Collision collisionInfo)
+    {
+        if(collisionInfo.collider.name=="Obstacle1")
+        {
+            Debug.Log("We hit obstacle1");
+        }
+        if (collisionInfo.collider.name == "Obstacle2")
+        {
+            Debug.Log("We hit obstacle2");
+        }
+        if (collisionInfo.collider.name == "Obstacle3")
+        {
+            Debug.Log("We hit obstacle3");
+        }
+        if (collisionInfo.collider.name == "Obstacle4")
+        {
+            Debug.Log("We hit obstacle4");
+        }
+        if (collisionInfo.collider.name == "Obstacle5")
+        {
+            Debug.Log("We hit obstacle5");
+        }
+        if (collisionInfo.collider.name == "Obstacle6")
+        {
+            Debug.Log("We hit obstacle6");
+        }
+        if (collisionInfo.collider.name == "Obstacle7")
+        {
+            Debug.Log("We hit obstacle7");
+        }
+        if (collisionInfo.collider.name == "Obstacle8")
+        {
+            Debug.Log("We hit obstacle8");
+        }
+        if (collisionInfo.collider.name == "Obstacle9")
+        {
+            Debug.Log("We hit obstacle9");
+        }
+        if (collisionInfo.collider.name == "Obstacle10")
+        {
+            Debug.Log("We hit obstacle10");
+        }
+        if (collisionInfo.collider.name == "Obstacle11")
+        {
+            Debug.Log("We hit obstacle11");
+        }
+        if (collisionInfo.collider.name == "Obstacle12")
+        {
+            Debug.Log("We hit obstacle12");
+        }
+        if (collisionInfo.collider.name == "Obstacle13")
+        {
+            Debug.Log("We hit obstacle13");
+        }
+        if (collisionInfo.collider.name == "Obstacle14")
+        {
+            Debug.Log("We hit obstacle14");
+        }
+        if (collisionInfo.collider.name == "Obstacle15")
+        {
+            Debug.Log("We hit obstacle15");
+        }
+        if (collisionInfo.collider.name == "Obstacle16")
+        {
+            Debug.Log("We hit obstacle16");
+        }
+        if (collisionInfo.collider.name == "Obstacle17")
+        {
+            Debug.Log("We hit obstacle17");
+        }
+        if (collisionInfo.collider.name == "Obstacle18")
+        {
+            Debug.Log("We hit obstacle18");
+        }
+        if (collisionInfo.collider.name == "Obstacle19")
+        {
+            Debug.Log("We hit obstacle19");
+        }
+        if (collisionInfo.collider.name == "Obstacle20")
+        {
+            Debug.Log("We hit obstacle20");
+        }
+        if (collisionInfo.collider.name == "Obstacle21")
+        {
+            Debug.Log("We hit obstacle21");
+        }
+        if (collisionInfo.collider.name == "Obstacle22")
+        {
+            Debug.Log("We hit obstacle22");
+        }
+        if (collisionInfo.collider.name == "Obstacle23")
+        {
+            Debug.Log("We hit obstacle23");
+        }
+        if (collisionInfo.collider.name == "Obstacle24")
+        {
+            Debug.Log("We hit obstacle24");
+        }
+        if (collisionInfo.collider.name == "Obstacle25")
+        {
+            Debug.Log("We hit obstacle25");
+        }
+        if (collisionInfo.collider.name == "Obstacle26")
+        {
+            Debug.Log("We hit obstacle26");
+        }
+        if (collisionInfo.collider.name == "Obstacle27")
+        {
+            Debug.Log("We hit obstacle27");
+        }
+        if (collisionInfo.collider.name == "Obstacle28")
+        {
+            Debug.Log("We hit obstacle28");
+        }
+        if (collisionInfo.collider.name == "Obstacle29")
+        {
+            Debug.Log("We hit obstacle29");
+        }
+        if (collisionInfo.collider.name == "Obstacle30")
+        {
+            Debug.Log("We hit obstacle30");
+        }
+
+    }
+}
